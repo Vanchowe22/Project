fc652474b7b1170362681868105dfe28e523a8ff
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2021-11-14 01:25:15 +0200

    refactor folders

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const router = {
+    '/home': <Main />
+  }
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
