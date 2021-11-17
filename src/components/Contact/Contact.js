e9c25c1dc1de03c89ce8d1229879539663f61c51
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2021-11-17 17:34:59 +0200

    refactor contact and changed dir

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,25 +11,25 @@ const Contact = () => {
                 <div class="col-lg-7 tm-contact-left">
                     <form class="mb-5 ml-auto mr-0 tm-contact-form">
                         <div class="form-group row mb-4">
-                            <label for="name" class="col-sm-3 col-form-label text-right tm-color-primary">Name</label>
+                            <label htmlFor="name" class="col-sm-3 col-form-label text-right tm-color-primary">Name</label>
                             <div class="col-sm-9">
                                 <input class="form-control mr-0 ml-auto" name="name" id="name" type="text" required />
                             </div>
                         </div>
                         <div class="form-group row mb-4">
-                            <label for="email" class="col-sm-3 col-form-label text-right tm-color-primary">Email</label>
+                            <label htmlFor="email" class="col-sm-3 col-form-label text-right tm-color-primary">Email</label>
                             <div class="col-sm-9">
                                 <input class="form-control mr-0 ml-auto" name="email" id="email" type="email" required />
                             </div>
                         </div>
                         <div class="form-group row mb-4">
-                            <label for="subject" class="col-sm-3 col-form-label text-right tm-color-primary">Subject</label>
+                            <label htmlFor="subject" class="col-sm-3 col-form-label text-right tm-color-primary">Subject</label>
                             <div class="col-sm-9">
                                 <input class="form-control mr-0 ml-auto" name="subject" id="subject" type="text" required />
                             </div>
                         </div>
                         <div class="form-group row mb-5">
-                            <label for="message" class="col-sm-3 col-form-label text-right tm-color-primary">Message</label>
+                            <label htmlFor="message" class="col-sm-3 col-form-label text-right tm-color-primary">Message</label>
                             <div class="col-sm-9">
                                 <textarea class="form-control mr-0 ml-auto" name="message" id="message" rows="8" required></textarea>
                             </div>
