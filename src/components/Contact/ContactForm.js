aeeafc9776d3cc492fff2bfaab6f34f6b816c46f
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2021-11-18 15:02:39 +0200

    Put new form for contact

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,26 +1,34 @@
 const ContactForm = () => {
     return (
-        <div class="row justify-content-center mt-5">
-            <div class="col-lg-8">
-                <form action="#" class="form-contact">
-                    <div class="row">
-                        <div class="col-sm-6 py-2">
-                            <input type="text" class="form-control" id="name" placeholder="Enter name.." />
-                        </div>
-                        <div class="col-sm-6 py-2">
-                            <input type="text" class="form-control" id="email" placeholder="Email address.." />
-                        </div>
-                        <div class="col-12 py-2">
-                            <input type="text" class="form-control" id="subject" placeholder="Subject.." />
-                        </div>
-                        <div class="col-12 py-2">
-                            <textarea id="message" rows="8" class="form-control" placeholder="Enter message.."></textarea>
-                        </div>
-                        <div class="col-12 mt-3">
-                            <button type="submit" class="btn btn-primary px-5">Submit</button>
-                        </div>
+        <div className="page-section">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="title-section mb-3">Stay in touch</h2>
+                    <p>Just say hello or drop us a line. You can manualy send us email on <a href="mailto:xBlog_firm@gmail.com">xBlog_firm@gmail.com</a></p>
+                </div>
+                <div className="row justify-content-center mt-5" style={{marginBottom:'50px'}}>
+                    <div className="col-lg-8">
+                        <form style={{padding:"40px"}} className="form-contact">
+                            <div className="row">
+                                <div className="col-sm-6 py-2">
+                                    <input type="text" className="form-control" id="name" placeholder="Enter name.." />
+                                </div>
+                                <div className="col-sm-6 py-2">
+                                    <input type="text" className="form-control" id="email" placeholder="Email address.." />
+                                </div>
+                                <div className="col-12 py-2">
+                                    <input type="text" className="form-control" id="subject" placeholder="Subject.." />
+                                </div>
+                                <div className="col-12 py-2">
+                                    <textarea id="message" rows="8" className="form-control" placeholder="Enter message.."></textarea>
+                                </div>
+                                <div className="col-12 mt-3">
+                                    <button type="submit" className="btn btn-primary px-5">Submit</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     );
