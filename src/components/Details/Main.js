6121bc235d0edf0929722e30106464445714731f
Vanchowe22 <79274004+Vanchowe22@users.noreply.github.com>
2021-12-09 21:04:03 +0200

    doesnt show buttons when u are not logged

--- src/components/Details/Main.js
+++ src/components/Details/Main.js
@@ -39,21 +39,23 @@ const Main = ({
     return (
         <div className="mb-4">
             {
-                article.owner && (article.owner._id == auth._id)
-                    ?
-                    <>
-                        <Link to={`/edit/${article._id}`} ><button style={{ 'position': 'absolute', 'right': '360px', 'top': '18px' }} className="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i className="fa fa-edit"></i></button></Link>
-                        <button onClick={onDelete} style={{ 'position': 'absolute', 'right': '320px', 'top': '18px' }} className="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i className="fa fa-trash"></i></button>
-                    </>
-                    :
-                    liked
-                        ? <button onClick={onUnlike} type="button" style={{ 'position': 'absolute', 'right': '255px', 'top': '15px' }} className="btn btn-dark">Unlike</button>
-                        : <button onClick={onLike} type="button" style={{ 'position': 'absolute', 'right': '255px', 'top': '15px' }} className="btn btn-primary">Like</button>
+                auth._id
+                    ? article.owner && (article.owner._id == auth._id)
+                        ?
+                        <>
+                            <Link to={`/edit/${article._id}`} ><button style={{ 'position': 'absolute', 'right': '360px', 'top': '18px' }} className="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i className="fa fa-edit"></i></button></Link>
+                            <button onClick={onDelete} style={{ 'position': 'absolute', 'right': '320px', 'top': '18px' }} className="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i className="fa fa-trash"></i></button>
+                        </>
+                        :
+                        liked
+                            ? <button onClick={onUnlike} type="button" style={{ 'position': 'absolute', 'right': '255px', 'top': '15px' }} className="btn btn-dark">Unlike</button>
+                            : <button onClick={onLike} type="button" style={{ 'position': 'absolute', 'right': '255px', 'top': '15px' }} className="btn btn-primary">Like</button>
+                    : ''
             }
 
-            <span style={{ 'position': 'absolute', 'right': '230px', 'top': '21px' }} >{article.likes ? article.likes.length : ''}</span>
+            <span style={{ 'position': 'absolute', 'right': '210px', 'top': '21px' }} >{article.likes ? article.likes.length : ''} likes</span>
             <h2 className="pt-2 tm-color-primary tm-post-title">{article.title}</h2>
-            <p className="tm-mb-40">{article.date} posted by {article.onwer ? article.owner.name : ''} </p>
+            <p className="tm-mb-40">{article.date} posted by {article.owner ? article.owner.name : ''} </p>
             <p>
                 {article.description}
             </p>
